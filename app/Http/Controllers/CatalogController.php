e4eacb0841b2897cb0d969bb6cb9a271e162c2c3
monther alzahar <mmonther020@gmail.com>
2025-06-26 14:06:49 +0300

    first commit

---
+++ app/Http/Controllers/CatalogController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CatalogController extends Controller
+{
+    function index() {
+        return view('index');
+    }
+
+    function about() {
+        return view('about');
+    }
+
+    function contact() {
+        return view('contact');
+    }
+
+    function photo_detail() {
+        return view('photo_detail');
+    }
+
+    function video_detail() {
+        return view('video_detail');
+    }
+
+    function videos() {
+        return view('videos');
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CatalogController;
+
+Route::get('/',[CatalogController::class,'index'])->name('index');
+Route::get('/about',[CatalogController::class,'about'])->name('about');
+Route::get('/contact',[CatalogController::class,'contact'])->name('contact');
+Route::get('/photo_detail',[CatalogController::class,'photo_detail'])->name('photo_detail');
+Route::get('/video_detail',[CatalogController::class,'video_detail'])->name('video_detail');
+Route::get('/videos',[CatalogController::class,'videos'])->name('videos');
